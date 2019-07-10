91373c4553aa9c241ba00295bb86de272b265135
Lars Trey <lars.trey@gmail.com>
2019-07-10 15:15:07 +0200

    cleanup commented code

--- src/modules/Cell.js
+++ src/modules/Cell.js
@@ -9,34 +9,4 @@ export default class Cell {
     this.state = state; // Dead (0) - Alive (1)
     this.color = Utils.getRandomColor();
   }
-
-  // isAlive() {
-  //   if (this.state === 1) {
-  //     return true;
-  //   }
-  //   return false;
-  // }
-
-  // isDead() {
-  //   if (this.state === 0) {
-  //     return true;
-  //   }
-  //   return false;
-  // }
-
-  // die() {
-  //   this.state = 0;
-  // }
-
-  // live() {
-  //   this.state = 1;
-  // }
-
-  // flickCell() {
-  //   if (this.isAlive()) {
-  //     this.die();
-  //   } else {
-  //     this.live();
-  //   }
-  // }
 }
